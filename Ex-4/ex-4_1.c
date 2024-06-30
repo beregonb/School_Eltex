9ffb35fbe55f7983fdca22f23185c1b28068eebe
beregonb <beregonb@student.21-school.ru>
2024-06-30 11:00:26 +0700

    finish ex_4

--- Ex-4/ex-4_1.c
+++ Ex-4/ex-4_1.c
@@ -32,6 +32,10 @@ void plus_abonent(struct abonent *abon, int *count) {
 }
 
 void print_abonent(struct abonent *abon, int count) {
+  if (count == 0) {
+    printf("Список пуст!!!\n");
+    return;
+  }
   for (int i = 0; i < count; i++) {
     printf("Имя: %s Фамилия: %s Телефон: %s\n", abon[i].name,
            abon[i].second_name, abon[i].tel);
